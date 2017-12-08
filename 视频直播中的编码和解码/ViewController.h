f8449403730ce8983b8bac192942c44097d9f2a5
gzp <zswangzp@163.com>
2017-12-08 17:41:42 +0800

    Initial Commit

---
+++ 视频直播中的编码和解码/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  视频直播中的编码和解码
+//
+//  Created by 管章鹏 on 2017/12/8.
+//  Copyright © 2017年 管章鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 视频直播中的编码和解码/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  视频直播中的编码和解码
+//
+//  Created by 管章鹏 on 2017/12/8.
+//  Copyright © 2017年 管章鹏. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
